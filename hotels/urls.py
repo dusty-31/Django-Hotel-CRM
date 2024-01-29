0bb0b203de237fbbcfc2907f3afce7c019dcdfec
Yuriy Rybak <jura.ryback007@gmail.com>
2024-01-29 23:23:34 +0200

    Added functionality to view one hotel.

--- hotels/urls.py
+++ hotels/urls.py
@@ -6,4 +6,5 @@ app_name = 'hotels'
 
 urlpatterns = [
     path('create/', views.HotelCreateView.as_view(), name='create'),
+    path('<int:pk>/', views.HotelDetailView.as_view(), name='detail'),
 ]
--- hotels/views.py
+++ hotels/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 from django.urls import reverse_lazy
 from django.http import HttpRequest, HttpResponse
 
-
+from .models import Hotel
 from .forms import HotelCreateForm
 from .services import create_rooms_for_hotel, save_hotel, create_hotel_rooms_count
 
@@ -43,3 +43,18 @@ class HotelCreateView(View):
         return render(request=request, template_name=self.template_name, context=context)
 
 
+class HotelDetailView(DetailView):
+    model = Hotel
+    template_name = 'hotels/hotel.html'
+    context_object_name = 'hotel'
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+        context['title'] = f'Hotel CRM - {self.object.name}'
+        return context
+
+    def get_object(self, **kwargs) -> Hotel:
+        hotel = get_object_or_404(klass=Hotel, pk=self.kwargs.get('pk'))
+        hotel.free_rooms = hotel.room_set.filter(is_available=True).count()
+        hotel.populated_rooms = hotel.room_set.filter(is_available=False).count()
+        return hotel
